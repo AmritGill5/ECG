34ae905b1e3c796677feb40c7812f08b92d6d846
AmritGill157 <83399881+AmritGill5@users.noreply.github.com>
2024-06-26 14:11:09 +0530

    Add files via upload

---
+++ 26mayemdmorph1.m
@@ -0,0 +1,202 @@
+clear
+close all
+clc
+
+fs = 360;                % sampling frequency (samples per second/Hz) 
+dt = 1/fs;               % seconds per sample 
+stopTime = 10;            % length of signal in seconds 
+t = (0:dt:stopTime-dt); % time vector in seconds 
+stopTime_plot = 2;       % limit time axis for improved visualization 
+
+
+
+
+% Load the data
+load('300m.mat');
+y = val;
+load('bwm.mat');
+n_s = val;
+
+% Normalize function to scale between -1 and 1
+%normalize_signal = @(x) 2 * (x - min(x)) / (max(x) - min(x)) - 1;
+
+% Normalize the signals
+%y = normalize_signal(y);
+%n_s = normalize_signal(n_s);
+
+% Create the noisy signal
+y_noise = y + n_s;
+%y_noise = normalize_signal(y_noise);  % Normalize the noisy signal
+noise_std = 0.2; 
+ensemble_count=100;
+
+% Normalize function to scale between -1 and 1
+normalize_signal = @(x) 2 * (x - min(x)) / (max(x) - min(x)) - 1;
+
+% Normalize the signals
+y2 = normalize_signal(y);
+n_s2 = normalize_signal(n_s);
+input_signal_power = rms(y).^2;  % Power of the input signal
+input_noise_power = rms(n_s).^2;    % Power of the input noise
+input_snr = 10 * log10(input_signal_power / input_noise_power);  % Input SNR in dB
+disp(['Signal-to-Noise Ratio (SNR): ', num2str(input_snr), ' dB']);
+
+% Using built-in functions if you have the toolbox
+mse_i = mean(( n_s2).^2);
+
+% Display the result
+disp(['Mean Squared Error (MSE) using immse: ', num2str(mse_i)]);
+
+
+% Initialize variables
+z = length(y);
+imfc = zeros(1, ensemble_count); % Array to store the number of IMFs for each ensemble
+imfs = [];
+
+% Perform EEMD
+for i = 1:ensemble_count
+    % Add white noise to the signal
+    noise = noise_std * randn(size(y));
+    noisy_signal = y_noise+ noise;
+    
+    % Perform EMD on the noisy signal
+    imf_temp = emd(noisy_signal, 'MaxNumIMF', 14);
+    
+    % Store the IMFs
+    if i == 1
+        % Initialize imfs array with the size based on the first EMD decomposition
+        imfs = zeros(ensemble_count, size(imf_temp, 1), size(imf_temp, 2));
+    end
+    
+    imfs(i, :, 1:size(imf_temp, 2)) = imf_temp;
+    imfc(1, i) = size(imf_temp, 2);
+end
+
+% Find the most common number of IMFs
+win_imf = mode(imfc);
+
+% Select only those ensembles with the most common number of IMFs
+matching_imfs = imfs(imfc == win_imf, :, 1:win_imf);
+
+% Average the selected IMFs across ensembles (1st dimension)
+avg_imfs = squeeze(mean(matching_imfs, 1));
+z_imf=avg_imfs';
+
+x = zeros(1,length(y));
+%m = round(0.24 * num_imfs);       
+for i = 1:win_imf- 3
+    x = x + z_imf(i,:);
+end   
+x_emd=y_noise-x;
+% Opening operation on x signal (example of a 3-point opening)
+se1 = strel('line', 0.38*fs, 0);  % Create a structuring element (line, length 3)
+x_o1 = imopen(x, se1); % Perform opening operation
+% Opening operation on x signal (example of a 3-point opening)
+  % Create a structuring element (line, length 3)
+x_c1= imclose(x, se1); % Perform opening operation
+ 
+ xmp1=(x_o1+x_c1)/2;
+ %=xmp1-x_n1;
+% Opening operation on x signal (example of a 3-point opening)
+se2= strel('line', 0.3*fs, 0);  % Create a structuring element (line, length 3)
+x_o2 = imopen(xmp1, se2); % Perform opening operation
+% Opening operation on x signal (example of a 3-point opening)
+%se4 = strel('line', 0.11*fs, 0);  % Create a structuring element (line, length 3)
+x_c2= imclose(xmp1, se2); % Perform opening operation
+xmp2=(x_o2+x_c2)/2;
+
+y_f2=x-xmp2;
+
+
+
+num=0;
+den=0;
+z=0;
+for i=1:length(y)
+    den=den+(y(i)-y_f2(i))^2;
+end
+
+for i=1:length(y)
+    num=num+y(i)^2;
+end
+
+
+SNR=20*log10(sqrt(num)/sqrt(den));
+
+disp(['Signal-to-Noise Ratio (SNR): ', num2str(SNR), ' dB']);
+
+y_f3 = normalize_signal(y_f2);
+
+% Calculate Mean Squared Error (MSE) after filtering
+mse_out = mean((y2- y_f3).^2);
+disp(['Mean Squared Error (MSE) after filtering: ', num2str(mse_out)]);
+
+
+
+% Display the number of IMFs
+num_imfs = size(avg_imfs, 2);
+fprintf('Number of IMFs: %d\n', num_imfs);
+
+figure;
+subplot(2, 1, 1);
+plot(t, y);
+title('Original ECG Signal');
+
+subplot(2, 1, 2);
+plot(t, y_f2);
+title('Filtured  ECG Signal');
+figure;
+subplot(6,1,1);
+plot(t,x);
+title('x_noise');
+xlabel('Time (samples)');
+ylabel('Amplitude');
+
+subplot(6,1,2);
+plot(t,xmp1);
+title('xmp1');
+xlabel('Time (samples)');
+ylabel('Amplitude');
+
+subplot(6,1,3);
+plot(t,x_o2);
+title('xmp2');
+xlabel('Time (samples)');
+ylabel('Amplitude');
+
+subplot(6,1,4);
+plot(t,y_f2);
+title(' xecg1  signal');
+xlabel('Time (samples)');
+ylabel('Amplitude');
+
+subplot(6,1,5);
+plot(t,n_s);
+title('noise signal');
+xlabel('Time (samples)');
+ylabel('Amplitude');
+
+subplot(6,1,6);
+plot(t,x_emd);
+title('baseline signal');
+xlabel('Time (samples)');
+ylabel('Amplitude');
+
+figure;
+plot(t,x);
+title('x_noise');
+xlabel('Time (samples)');
+ylabel('Amplitude');
+
+
+% Plot the original signal and its averaged IMFs
+figure;
+subplot(num_imfs + 1, 1, 1);
+plot(t, y);
+title('Original ECG Signal');
+
+for k = 1:num_imfs
+    subplot(num_imfs + 1, 1, k + 1);
+    plot(t, avg_imfs(:, k));
+    title(['IMF ', num2str(k)]);
+end
